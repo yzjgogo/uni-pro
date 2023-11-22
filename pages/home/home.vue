6772ae97b765af9c354d919277a92ae1ff37bde3
yinzhijie <yzjgogo@139.com>
2023-11-22 17:04:14 +0800

    添加tabbar

---
+++ pages/home/home.vue
@@ -0,0 +1,21 @@
+<template>
+	<view>
+		Home
+		HX这边如果删除了某些东西，比如这里删除了创建项目自带的pages/index页面，则你要么在微信开发者工具里也手动去删除index页面，要么把微信开发者工具关掉，然后HX重新：运行->运行到小程序模拟器->微信开发者工具
+		否则会报错
+	</view>
+</template>
+
+<script>
+	export default {
+		data() {
+			return {
+				
+			};
+		}
+	}
+</script>
+
+<style lang="scss">
+
+</style>
